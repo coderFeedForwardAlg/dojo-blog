475fcce74ed47662b9297d3a66e0e94ca7ad6d97
coderFeedForwardAlg <76785692+coderFeedForwardAlg@users.noreply.github.com>
2022-05-24 15:36:05 -0400

    Add files via upload

---
+++ dojo-blog/src/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <h1> Dojo Blog </h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create">new blog</a>
+                <a href="/cool link">cool link</a>
+            </div>
+        </nav> 
+     );
+}
+ 
+export default Navbar;
