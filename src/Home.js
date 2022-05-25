982d2e6dfb94599f0b4cba178b5315701ae1c234
coderFeedForwardAlg <gaspersscottma@gmail.com>
2022-05-25 15:10:16 +0200

    secont commit

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,41 @@
-import { useState } from "react";
+import { useState , useEffect} from "react";
+import BlogList from "./BlogList";
+
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(
-        [
-            {title: "how to ninja" , body: "lorem ipsum...", auther: "mario", id: 1},
-            {title: "how to code" , body: "lorem ipsum...", auther: "luigi", id: 1},
-            {title: "how to ninja-code" , body: "lorem ipsum...", auther: "bob", id: 1}
-        ]
-    )
-    
+    const [blogs, setBlogs] = useState(null);
+
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        setTimeout(() => {
+            fetch("http://localhost:8000/blogs ")
+            .then( (res) => {
+                if(!res.ok){
+                    throw Error("could not fetch data"); 
+                }
+                return res.json();
+            })
+            .then( (data) => {
+                setBlogs(data);
+                setIsLoading(false);
+                setError(null);
+            })
+            .catch( (e) => { 
+                setIsLoading(false);
+                setError(e.message);
+            }); 
+        }, 1000)
+    }, []);  
+ 
+
+
     return ( 
         <div className="home">
-            
+          {error && <p> {error} </p>}
+          { blogs &&  <BlogList blogs = {blogs} title= "All Blogs!" />  }
+          {isLoading && <div>loading ... </div> }
         </div>
      );
 }
